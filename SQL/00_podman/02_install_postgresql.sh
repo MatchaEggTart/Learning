5c15baee824a11ec3174cb08e3031be4034d5f95
MatchaEggTart <MatchaEggTart@hotmail.com>
2026-01-25 04:27:42 +0800

    Update

--- SQL/00_podman/02_install_postgresql.sh
+++ SQL/00_podman/02_install_postgresql.sh
@@ -8,7 +8,8 @@ mkdir -p PostgreSQL
 cd PostgreSQL
 
 # 从脚本所在目录复制 docker-compose.yml
-cp "$SCRIPT_DIR/postgresql.yml" ./docker-compose.yml
+cp "$SCRIPT_DIR/postgresql_volume.yml" ./postgresql.yml
 cp "$SCRIPT_DIR/.env" ./.env
 
-podman-compose up -d
+podman compose -f postgresql.yml up -d
+# podman-compose up -d # 需要yml名字是 docker-compose.yml
---
+++ SQL/00_podman/delete_container.sh
@@ -0,0 +1,4 @@
+podman ps -a
+podman rm -f <CONTAINER ID>
+rm -rf <映射文件>
+podman network rm <特定网络配置名>
